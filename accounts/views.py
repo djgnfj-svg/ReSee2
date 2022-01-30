bcea792dd91aa5766839abc502c29f6bee3ed1b3
djgnfj-svg <djgnfj8923@naver.com>
2022-01-31 00:23:47 +0900

    actuate login register

--- accounts/views.py
+++ accounts/views.py
@@ -1,47 +1,73 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
 
-from accounts.form import LoginForm, MemberDelForm, MemberUpdateForm, RegisterForm
+from accounts.form import MemberDelForm, MemberUpdateForm
 from accounts.models import MyUser
 
 # Create your views here.
 @csrf_exempt
 def login_view(request):
     if request.method == "POST":
-        form_login = LoginForm(request.POST)
-        if form_login.is_valid():
-            email = form_login.cleaned_data.get("email")
-            raw_password = form_login.cleaned_data.get("password")
-            msg = "가입되있지 않습니다."
+        user_email = request.POST.get("user_email")
+        user_password = request.POST.get("user_password")
+        res_data = {}
+        if not (user_email and user_password):
+            res_data['error'] = "모든 값을 입력해야 합니다."
         try :
-            user = MyUser.objects.get(email=email)
+            user = MyUser.objects.get(email=user_email)
         except MyUser.DoesNotExist:
             pass
         else :
-            if user.check_password(raw_password):
-                msg = "성공"
+            if user.check_password(user_password):
                 login(request, user)
-                return render(request,"home.html")
-    else:
-        msg = None
-        form_login = LoginForm()
-    return render(request, "login.html", {"form_login" : form_login, "msg" : msg})
+                return redirect("home")
+    return render(request, "login.html")
 
 def logout_view(request):
     logout(request)
     return redirect("home")
-
+@csrf_exempt
 def register_view(request):
-	form = RegisterForm()
-	if request.method == "POST":
-		form = RegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			msg = "가입완료"
-		return render(request, "home.html", msg)
-	return render(request, "register.html", {"form" : form})
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('user_name',None)
+        useremail = request.POST.get('register_email',None)
+        password = request.POST.get('register_password',None)
+        repasswd = request.POST.get('confirm_password',None)
+
+        res_data ={}
+        if not (username and useremail and password and repasswd):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        elif password != repasswd:
+            res_data['error'] ='비밀번호가 다릅니다.'
+
+        try:
+            user = MyUser.objects.get(email=useremail)
+            if user:
+                res_data['status'] = '0' # 기존 가입된 회원
+                # return JsonResponse(res_data)
+                return redirect("home")
+        except MyUser.DoesNotExist:
+            # 대문자 User 임에 주의
+            user = MyUser(
+                nickname = username,
+                email = useremail,
+                password = make_password(password)
+            )
+            user.save()
+            # session 생성
+            user = MyUser.objects.get(email=useremail)
+            request.session['user'] = user.id
+            res_data['status'] = '1' # 회원 가입 완료
+            # return JsonResponse(res_data)
+            return redirect("home")
+
+        return render(request,'register.html',res_data)
 
 @csrf_exempt
 @login_required
